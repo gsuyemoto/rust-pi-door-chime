7edaf7deca8f8c7ef9b522dc4228f42757bba8ec
Gary <gsuyemoto@gmail.com>
2021-03-03 23:48:19 -0800

    Refactored out the functionality to find and load sound files. Now a function looks for files with a given string in the filename and puts them in a vector to be randomly selected for playing when needed. Using fastrand crate for random number generation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::error::Error;
+use std::vec::Vec;
+use std::path::Path;
 use rust_gpiozero::*;
 use soloud::*;
+use glob::glob;
+use fastrand::*;
 
 #[derive(Clone, Copy, Debug)]
 enum State {
@@ -18,44 +22,52 @@ fn get_state(mag_state: bool, pir_state: bool) -> State {
     }
 }
 
+fn get_sound_files(list_snds: &mut Vec<Wav>, search: &str) {
+    let files_path = format!("/home/pi/workspace/rust-pi-door-chime/media/*{}*.wav", search);
+    
+    for entry in glob(&files_path).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path_buf)    => { 
+                let mut new_sound = Wav::default();
+                new_sound.load(&path_buf.as_path());
+
+                list_snds.push(new_sound); 
+                println!("File: {:?}", path_buf.display()); 
+            },
+            Err(e)      => println!("Error: {:?}", e),
+        }
+    }
+}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
 	let mut last_state  = State::DoorClosed;
-	let mag 		    = InputDevice::new(23);
-	let pir 		    = InputDevice::new(24);
-	let mut mag_led 	= LED::new(25);
-	let pir_led 	    = LED::new(18);
 	
+    let mag 		    = InputDevice::new(23);
+	let pir 		    = InputDevice::new(24);
+	let mag_led 	    = LED::new(25);
+	let pir_led 	    = LED::new(18);
+
+    let mut snds_enter  = Vec::new();
+    let mut snds_exit   = Vec::new();       	
+
+    get_sound_files(&mut snds_enter, "enter");
+    get_sound_files(&mut snds_exit, "exit");
+
 	pir_led.off();
 	mag_led.off();
 	
-	let mut enter = audio::Wav::default();
-	enter.load(&std::path::Path::new("/home/pi/workspace/rust-pi-door-chime/media/enter.wav")).unwrap_or_else( |err| {
-		mag_led.blink(2.0, 3.0);
-		println!("Unable to load .wav file: {:?}", err);
-		loop {}
-	});
-
-	let mut exit = audio::Wav::default();
-	exit.load(&std::path::Path::new("/home/pi/workspace/rust-pi-door-chime/media/exit.wav")).unwrap_or_else( |err| {
-		mag_led.blink(2.0, 3.0);
-		println!("Unable to load .wav file: {:?}", err);
-		loop {}
-	});
-
-	let sl = Soloud::default().unwrap_or_else( |err| {
-		mag_led.blink(2.0, 3.0);
-		println!("Unable to create soloud: {:?}", err);
-		loop {}
-	});
+	let sl = Soloud::default()?;
 
 	loop {
-        let current_state = get_state(mag.is_active(), pir.is_active());
+        let current_state   = get_state(mag.is_active(), pir.is_active());
+        let random_enter    = fastrand::usize(..snds_enter.len());
+        let random_exit     = fastrand::usize(..snds_exit.len());
 
         println!("last state: {:?}", last_state);
         match (current_state, last_state) {
             (State::DoorOpenEntering, State::DoorClosed) => {
                 println!("entering");
-                sl.play(&enter);
+                sl.play(&snds_enter[random_enter]);
                 mag_led.on();
 
 			    while sl.voice_count() > 0 {	
@@ -64,7 +76,7 @@ pub fn run() -> Result<(), Box<dyn Error>> {
             },
             (State::DoorOpenExiting, State::DoorClosed) => {
                 println!("exiting");
-                sl.play(&exit);
+                sl.play(&snds_exit[random_exit]);
                 mag_led.on();
                 pir_led.on();
 			    
@@ -77,9 +89,19 @@ pub fn run() -> Result<(), Box<dyn Error>> {
                 mag_led.off();
                 pir_led.off();
             },
-            _ => println!("Do nothing as door is already open"),
+            _ => println!("door never closed, do nothing"),
         }
 
         last_state = current_state;
 	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_sound_empty_string() {
+        
+    }
 }
