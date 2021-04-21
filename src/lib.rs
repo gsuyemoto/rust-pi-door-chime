55494516a7d7f47f38f7bc171780de1d452e0811
Gary <gsuyemoto@gmail.com>
2021-04-20 17:10:48 -0700

    Removed sensor to detect whether exiting or entering. Changed audio from Soloud library to instead make a system call to aplay.

--- src/lib.rs
+++ src/lib.rs
@@ -7,27 +7,24 @@ use std::
     time::Duration,
     time::Instant,
     thread,
-    sync::mpsc,
+    process::Command,
+    path::PathBuf,
 };
 
 use rust_gpiozero::*;
-use soloud::*;
 use glob::glob;
 
 const DEFAULT_VOLUME: f32           = 1.0;
-const DEFAULT_DISTANCE: u128        = 60;
 const FILE_CONFIG: &str             = "config.txt";
-const FILE_PATH: &str               = "/mnt/usbdrive"; 
+const FILE_PATH: &str               = ""; 
 const FILE_SOUND_PATH: &str         = "/media"; 
 const FILE_SOUND_TYPE: &str         = ".wav"; 
-const FILE_SOUND_ENTER: &str        = "enter"; 
-const FILE_SOUND_EXIT: &str         = "exit"; 
-const FILE_SOUND_GREETING: &str     = "You're entering Sunmerry!"; 
-const FILE_SOUND_PARTING: &str      = "Have a great day!"; 
+const PIN_MAG: u8                   = 13;
+const PIN_LED1: u8                  = 5;
+const PIN_LED2: u8                  = 6;
 
 pub struct Config {
     pub volume: f32,
-    pub distance: u128,
 }
 
 impl Config {
@@ -37,7 +34,7 @@ impl Config {
         if args_vec.len() < 2 {
             args_vec = match fs::read_to_string(format!("{}/{}", FILE_PATH, FILE_CONFIG)) {
                 Ok(contents)    => contents.split_whitespace().map(str::to_string).collect(),
-                Err(_)          => vec![DEFAULT_VOLUME.to_string(), DEFAULT_DISTANCE.to_string()],
+                Err(_)          => vec![DEFAULT_VOLUME.to_string()],
             };
         }
         else {
@@ -53,20 +50,10 @@ impl Config {
                             },
         };
 
-        let distance: u128  = match args_vec[1].parse::<u128>() {
-            Ok(arg)         => arg,
-            Err(e)          => {
-                                    println!("Error parsing distance: {}", e);
-                                    DEFAULT_DISTANCE
-                            },
-        };
-
         println!("Volume: {}", volume);
-        println!("Distance: {}", distance);
 
         Config {
             volume,
-            distance,
         }
     }
 }
@@ -87,90 +74,14 @@ fn get_state(mag_state: bool, person_detected: bool) -> State {
     }
 }
 
-fn median(mut readings: Vec<u128>) -> u128 {
-    readings.sort();
-    let mid = readings.len() /2;
-    readings[mid]
-}
-
-struct DetectPerson {
-    worker:             Option<thread::JoinHandle<()>>,
-    person_detected:    bool,
-}
-
-impl DetectPerson {
-    fn new(min_distance: u128, pin_out: OutputDevice, pin_in: InputDevice, sender: mpsc::Sender<bool>) -> DetectPerson {
-        let mut last_ten: Vec<u128> = Vec::with_capacity(10);
-
-        let worker = thread::spawn(move || loop {
-            let distance = Self::get_sensor_readings(pin_out, pin_in);
-            last_ten.push(distance);
-
-            if last_ten.len() == 10 {
-                let median = median(last_ten.clone());
-                sender.send(median < min_distance);
-                last_ten.pop();
-                println!("median: {}", median);
-            }
-
-            // wait 60 ms between measurements
-            sleep(Duration::from_millis(60));
-        });
-
-        DetectPerson {
-            worker: Some(worker),
-            person_detected: false,
-        }
-    }
-
-    fn get_sensor_readings(pin_out:OutputDevice, pin_in: InputDevice) -> u128 {
-        // send sonic
-        pin_out.on();
-        sleep(Duration::from_micros(10));
-        pin_out.off();
-        
-        // measure
-        let check_fail      = Instant::now();
-        let mut did_fail    = false;
-        while !pin_in.is_active() { 
-            if check_fail.elapsed().as_micros() > 1700 {
-                did_fail = true;
-                break; 
-            }                
-        }
-        
-        if did_fail {
-            println!("Failed...");
-            sleep(Duration::from_millis(60));
-            return 0
-        }
-        
-        let time_start      = Instant::now();
-        
-        while pin_in.is_active() {}
-        let time_elapsed    = time_start.elapsed().as_micros();
-        let distance        = time_elapsed / 148; // for inches
-        // println!("Distance: {:?}", distance);
-        
-        distance        
-    }
-}
-
-
-struct Sound( Option<Wav>, Option<Speech>);
-
-fn get_sound_files(list_snds: &mut Vec<Sound>, search: &str) {
-    let files_path = format!("{}{}/{}*{}", FILE_PATH, FILE_SOUND_PATH, search, FILE_SOUND_TYPE);
+fn get_sound_files(list_snds: &mut Vec<PathBuf>) {
+    let files_path = format!("{}{}/*{}", FILE_PATH, FILE_SOUND_PATH, FILE_SOUND_TYPE);
  
     for entry in glob(&files_path).expect("Failed to read files") {
         match entry {
             Ok(path_buf)    => { 
                 println!("File: {}", path_buf.display());
-
-                let mut new_sound = Wav::default();
-                new_sound.load(&path_buf.as_path()).expect("Unable to load a sound file");
-
-                list_snds.push(Sound(Some(new_sound), None)); 
+                list_snds.push(path_buf); 
             },
             Err(e)      => println!("Error: {:?}", e),
         }
@@ -180,32 +91,13 @@ fn get_sound_files(list_snds: &mut Vec<Sound>, search: &str) {
 pub fn run() -> Result<(), Box<dyn Error>> {
     let config          = Config::new();
 	let mut last_state  = State::DoorClosed;
-	
-    let trig            = OutputDevice::new(5);
-    let echo            = InputDevice::new(6);
-    let mag 		    = InputDevice::new(23);
+    let mag 		    = InputDevice::new(PIN_MAG);
+	let mag_led 	    = LED::new(PIN_LED1);
+	let ppl_led 	    = LED::new(PIN_LED2);
 
-	let mag_led 	    = LED::new(25);
-	let ppl_led 	    = LED::new(18);
+    let mut sounds: Vec<PathBuf>  = Vec::new();
+    get_sound_files(&mut sounds);
 
-    let mut snds_enter: Vec<Sound>  = Vec::new();
-    let mut snds_exit: Vec<Sound>   = Vec::new();       	
-
-    get_sound_files(&mut snds_enter, FILE_SOUND_ENTER);
-    get_sound_files(&mut snds_exit, FILE_SOUND_EXIT);
-
-    if snds_enter.len() == 0 {
-        let mut speech = Speech::default();
-        speech.set_text(FILE_SOUND_GREETING)?;
-        snds_enter.push(Sound(None, Some(speech)));
-    }
-
-    if snds_exit.len() == 0 {
-        let mut speech = Speech::default();
-        speech.set_text(FILE_SOUND_PARTING)?;
-        snds_exit.push(Sound(None, Some(speech)));
-    }
-    
     ppl_led.on();
     mag_led.on();
     
@@ -214,53 +106,30 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 	ppl_led.off();
 	mag_led.off();
 	
-	let mut sl = Soloud::default().expect("Unable to create Soloud");
-    sl.set_global_volume(config.volume);
-
-    let (tx, rx)    = mpsc::channel(); 
-    let _           = DetectPerson::new(
-                        config.distance,
-                        trig,
-                        echo,
-                        tx );
-
 	loop {
-        let person_was_detected = match rx.try_recv() {
-            Ok(msg)     => msg,
-            Err(_)      => false,
-        };
+        let current_state   = get_state(mag.is_active(), false);
+        let random_sound    = fastrand::usize(..sounds.len());
+        let sound_path      = sounds
+                                .get_mut(random_sound)
+                                .unwrap();
 
-        let current_state   = get_state(mag.is_active(), person_was_detected);
-        let random_enter    = fastrand::usize(..snds_enter.len());
-        let random_exit     = fastrand::usize(..snds_exit.len());
+        let sound_path_str  = sound_path
+                                .to_str()
+                                .unwrap();
 
         match (current_state, last_state) {
             (State::DoorOpenEntering, State::DoorClosed) => {
-                // println!("entering: {:?} : {:?}", current_state, last_state);
-                match &snds_enter[random_enter].0 {
-                    Some(sound)     => sl.play(sound),
-                    None            => sl.play(snds_enter[random_enter].1.as_ref().unwrap()),
-                };
+                println!("entering: {:?} : {:?}", current_state, Command::new("aplay").arg(sound_path_str).status());
                 
                 mag_led.on();
-
-			    while sl.voice_count() > 0 {	
-			    	sleep(Duration::from_millis(100));
-			    }
+			   	sleep(Duration::from_millis(100));
             },
             (State::DoorOpenExiting, State::DoorClosed) => {
-                // println!("exiting: {:?} : {:?}", current_state, last_state);
-                match &snds_exit[random_exit].0 {
-                    Some(sound)     => sl.play(sound),
-                    None            => sl.play(snds_exit[random_exit].1.as_ref().unwrap()),
-                };
+                println!("entering: {:?} : {:?}", current_state, Command::new("aplay").arg(sound_path_str).status());
 
                 mag_led.on();
                 ppl_led.on();
-			    
-                while sl.voice_count() > 0 {	
-			    	sleep(Duration::from_millis(100));
-			    }
+			   	sleep(Duration::from_millis(100));
             },
             (State::DoorClosed, _) => {
                 // println!("door closed");
@@ -271,6 +140,7 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         }
 
         last_state = current_state;
+        sleep(Duration::from_secs(3));
 	} 
 }
 
